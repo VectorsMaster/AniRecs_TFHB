e97180040d59fe0e2c7aa5d47b5968862c414da1
Ghadeer Akleh <g.akleh@innopolis.university>
2024-04-27 05:28:32 +0300

    Added flake8 and bandit

--- Backend/routers/items.py
+++ Backend/routers/items.py
@@ -6,6 +6,7 @@ from schemas import ItemCreate, ItemResponse
 
 router = APIRouter()
 
+
 # Dependency to get the database session
 def get_db():
     db = SessionLocal()
@@ -29,8 +30,7 @@ async def create_item(item: ItemCreate, db: Session = Depends(get_db)):
 @router.get("/items/{item_id}", response_model=ItemResponse)
 def read_item(item_id: int, db: Session = Depends(get_db)):
     db_item = db.query(Item).filter(Item.id == item_id).first()
-
     if db_item is None:
         raise HTTPException(status_code=404, detail="Item not found")
-    
+
     return db_item
