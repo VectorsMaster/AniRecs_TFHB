24e716e95b89d2ef90f9e53e042c08773bf1139e
Ghadeer Akleh <g.akleh@innopolis.university>
2024-04-27 04:06:10 +0300

    Backed files structure

--- Backend/main.py
+++ Backend/main.py
@@ -3,16 +3,11 @@ from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 from pydantic import BaseModel
+from database import engine, SessionLocal, Base
+
 
 # FastAPI app instance
 app = FastAPI()
-
-# Database setup
-DATABASE_URL = "sqlite:///./test.db"
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
-
 
 # Database model
 class Item(Base):
