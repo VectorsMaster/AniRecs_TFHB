f9b9452d26ca29c12b8c90054115227e70755f1d
Ahmad Sarhan <ahmadhasansarhana@gmail.com>
2024-04-28 02:06:29 +0300

    JWT authentication

--- anirecs/backend/main.py
+++ anirecs/backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from database import engine, Base
-from routers import animes
+from routers import animes, users
 
 app = FastAPI()
 
@@ -9,6 +9,7 @@ Base.metadata.create_all(bind=engine)
 
 # Include routers
 app.include_router(animes.router)
+app.include_router(users.router)
 
 if __name__ == "__main__":
     import uvicorn
---
+++ anirecs/backend/routers/users.py
@@ -0,0 +1,102 @@
+from datetime import datetime, timedelta, timezone
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from sqlalchemy.orm import Session
+from secrets import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
+
+from schemas.users import Token, TokenData, UserResponse
+from models import User
+from database import get_db
+
+router = APIRouter()
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+def get_user(db, username: str):
+    user = db.query(User).filter(User.username == username).first()
+    if user:
+        return user
+    return False
+
+def authenticate_user(db, username: str, password: str):
+    user = get_user(db, username)
+    
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = get_user(username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+@router.post("/token")
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: Session = Depends(get_db)
+) -> Token:
+    user = authenticate_user(db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return Token(access_token=access_token, token_type="bearer")
+
+
+@router.post("/sign_in/", response_model=UserResponse)
+async def sign_user_in(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db : Session = Depends(get_db)):
+    user = get_user(db, form_data.username)
+    if user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            details="You are already signed in"
+        )
+    
+    user = User(username=form_data.username, hashed_password=get_password_hash(form_data.password))
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return user
